419336f1437863fff63a83d3c605986c18b904ae
Alkeshkumar94 <alkeshkumar949@gmail.com>
2024-08-07 10:57:26 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,71 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App =()=>{
+    const [input1,setInput1]=useState("");
+    const [input2,setInput2]=useState("");
+    const [result,setResult]=useState("");
 
+    const input1Handle=(e)=>{
+        setInput1(e.target.value);
+    }
+    const input2Handle=(e)=>{
+        setInput2(e.target.value);
+    }
+    
+    const calculateRelation=()=>{
+        let string1=input1;
+    let string2=input2;
+     if(!input1 || !input2){
+      setResult("Please Enter valid input");
+      return;
+     }
+
+     for(let char of string1){
+        if(string2.includes(char)){
+            string1=string1.replace(char,"");
+            string2=string2.replace(char,"");
+        }
+     }
+     const length=(string1.length+string2.length)%6;
+
+     let relation="";
+     switch(length){
+        case 1: relation="Friends";
+        break;
+        case 2: relation="Love";
+        break;
+        case 3: relation="Affection";
+        break;
+        case 4: relation="Marriage";
+        break;
+        case 5: relation="Enemy";
+        break;
+        case 0: relation="Siblings";
+        break;
+     }
+  setResult(relation);
+    }
+    const clear=()=>{
+        setInput1("");
+        setInput2("");
+        setResult("");
+        
+    }
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+              <input data-testid="input1" value={input1} onChange={input1Handle} placeholder="Enter first name" />
+              <input data-testid="input2" value={input2} onChange={input2Handle} placeholder="Enter second name"/>
+              <button data-testid="calculate_relationship" onClick={calculateRelation}>Calculate Relationship Future</button>
+              <button data-testid="clear" onClick={clear}>Clear</button>
+
+              <h3>
+                {
+                    result
+                }
+              </h3>
             </div>
         )
-    }
 }
 
 
